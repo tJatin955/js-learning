b1f2f9f1693e250a773b6b813723e3e518fc3453
tJatin955 <117781765+tJatin955@users.noreply.github.com>
2024-09-28 13:13:09 +0000

    ConversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33;
+let scores = "33abc";
+
+//console.log("33 is treated as a number ");
+
+//console.log(typeof (score));
+//console.log("33 is treated as a string ");
+
+//console.log(typeof (scores));
+
+let valueInNumber = Number(scores)
+//console.log("now string is converted into a number");
+
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);  //  NaN is abbreviation of Not a Number
+
+let isBoolean = "1";
+let isBoolearOr = Boolean(isBoolean);
+console.log(isBoolearOr);
