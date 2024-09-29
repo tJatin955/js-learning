176ca8bc56d926bd27136363f546a4cf97f01dc4
tJatin955 <117781765+tJatin955@users.noreply.github.com>
2024-09-29 17:47:30 +0000

    01_Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+//   ARRAYS
+
+const arr = [0, 1, 2, 3, 4, 5]
+console.log(arr[0])
+console.log(arr)
+
+
+arr.push(12)
+console.log(arr);
+
+
+arr.pop()
+console.log(arr)
+
+
+arr.unshift(100)
+console.log(arr)
+
+
+arr.shift()
+console.log(arr)
