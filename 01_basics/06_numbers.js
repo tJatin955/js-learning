456bc7e9446a3055925f1fef4e417fa43d41143a
tJatin955 <117781765+tJatin955@users.noreply.github.com>
2024-09-29 17:03:41 +0000

    Number & Maths

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,31 @@
+const score = 30;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance)
+
+// console.log(balance.toString.length)
+// console.log(balance.toFixed(2));
+
+// const otherName = 123.4561;
+// console.log(otherName.toPrecision(4))
+
+// const rupees = 1000000;
+// console.log(rupees.toLocaleString('en-IN'))
+
+
+
+// *********************MATHS****************************
+
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,9,2,8))
+console.log(Math.max(4,9,2,8))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
